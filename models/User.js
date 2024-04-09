19b9d4b2a1859fd316b5f56e75c71bc98988948d
ZeuS <ponzeadrian@gmail.com>
2024-04-09 13:13:51 +1000

    added comments to user model

--- models/User.js
+++ models/User.js
@@ -2,30 +2,36 @@ const { Schema, model, Types } = require('mongoose');
 
 const userSchema = new Schema(
   {
+    // Field for the username
     username: {
       type: String,
       unique: true,
       required: true,
+      // Trims whitespace from the username
       trim: true
     },
+    // Field for the email
     email: {
       type: String,
       required: true,
       unique: true,
-      // Built in validation for email
+      // Built in validation for email using Regex
       validate: {
         validator: function (v) {
           return /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
         },
+        // Custom error message for invalid email
         message: (props) => `${props.value} is not a valid email address!`
       }
     },
+    // Field for storing the IDs of thoughts associated with the user
     thoughts: [
       {
         type: Schema.Types.ObjectId,
         ref: 'Thought'
       }
     ],
+    // Field for storing the IDs of other users (friends) associated with the user
     friends: [
       {
         type: Schema.Types.ObjectId,
