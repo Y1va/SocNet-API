8056f5ae351192f5c2ba12e50a825af5b60177fd
ZeuS <ponzeadrian@gmail.com>
2024-04-09 11:29:54 +1000

    set up connection for model index.js and model file structure

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
 // Importing required modules from mongoose library
 const { connect, connection } = require('mongoose');
 
+
 // Defining the connection string for MongoDB
-const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
+const connectionString = 'mongodb://127.0.0.1:27017/usersDB';
+
 
 // Establishing the connection to the MongoDB databse using the connection string
 connect(connectionString);
+
 
 // Exporting the connection object for external use
 module.exports = connection;
