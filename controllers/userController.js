e810cbb0c4224a97086883308e21635b50fc00ea
ZeuS <ponzeadrian@gmail.com>
2024-04-09 18:28:24 +1000

    Created routes for creating and updating a user in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,13 +17,46 @@ module.exports = {
   async getSingleUser(req, res) {
     const { userId } = req.params;
     try {
-      const user = await User.findById(userId);
+      const user = await User.findOne(userId);
       if (!user) {
-        return res.status(404).json({ message: 'User not found with that ID!'})
+        return res
+          .status(404)
+          .json({ message: 'User not found with that ID!' });
       }
       res.json(user);
     } catch (err) {
       console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  // Create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  // Update a user by its id
+  async updateUser(req, res) {
+    const { userId } = req.params;
+    const { username, email } = req.body;
+    try {
+      const user = await User.findOneAndUpdate(
+        userId,
+        { username, email },
+        { runValidators: true, new: true }
+      );
+      if(!user) {
+        return res.status(404).json({ message: 'Cannot update user with this ID'})
+      }
+      res.json(user)
+    } catch (err) {
+      console.error(err)
       res.status(500).json({ error: 'Internal Server Error'})
     }
   }
