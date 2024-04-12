8321a84e58a68a2a99bef24b288c943af8e83cf3
ZeuS <ponzeadrian@gmail.com>
2024-04-13 09:27:51 +1000

    Created routes for thoughtRoutes file

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+// Routes for /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// Routes for /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// Routes for /api/thoughts/:thoughtId/reactions
+router.route('/thoughtId/reactions').post(createReaction);
+
+// Routes for /api/thoughts/:thoughtId/reactions/reactionId
+router.route('thoughtId/reactions/reactionId').delete(deleteReaction);
+
+module.exports = router;
