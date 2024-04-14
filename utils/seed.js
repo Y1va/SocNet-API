32a93e6570bf0f5bde7765362127e1a700648cf2
ZeuS <ponzeadrian@gmail.com>
2024-04-14 10:05:38 +1000

    wrote script to seed reactions

--- utils/seed.js
+++ utils/seed.js
@@ -18,7 +18,10 @@ async function seedDataBase() {
 
     // Associate thoughts with users
     const thoughtsWithUsers = thoughts.map((thought) => {
+      // Map each thought to its corresponding user
+      // Find the user associated with the thought
       const user = seedUsers.find((user) => user.username === thought.username);
+      // Assign the user's _id to the thoughts userId field
       thought.userId = user._id;
       return thought;
     });
@@ -26,7 +29,26 @@ async function seedDataBase() {
     // Seed the thoughts
     await Thought.insertMany(thoughtsWithUsers);
 
-
+    // Seed the reactions for each thought
+    for (const thought of seedThoughts) {
+      // Iterate over each thought
+      const reactions = thought.reactions.map((reaction) => {
+        // Map each reaction to its corresponding user
+        const user = seedUsers.find(
+          (user) => user.username === reaction.username
+        );
+        return {
+          // Copy the reaction body
+          reactionBody: reaction.reactionBody,
+          // Assign the users _id to the reactions username field
+          username: user._id
+        };
+      });
+      // Update the thought's reactions with the mapped reactions
+      thought.reactions = reactions
+      // Save the modified thought back to the database
+      await thought.save()
+    }
 
     console.log('Database seeded successfully');
   } catch (err) {
